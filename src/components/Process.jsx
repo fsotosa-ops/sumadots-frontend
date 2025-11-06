3c9164bd5c633e4dd25a4bbd88847b14eaa1cd8e
felipe-agustin <hola@felipeagustin.com>
2025-11-06 19:24:33 -0300

    ajuste dimension 160x160 suma icon Process.jsx

--- src/components/Process.jsx
+++ src/components/Process.jsx
@@ -125,10 +125,10 @@ function Process() {
               {/* El ícono ahora es más grande (120x120) y está centrado (140, 140) */}
               <image 
                 href={logoSumaIcon} 
-                x="110" 
-                y="110" 
-                height="180" 
-                width="180" 
+                x="120" 
+                y="120" 
+                height="160" 
+                width="160" 
                 className="process-center-icon"
               />
               {/* ========================================= */}
